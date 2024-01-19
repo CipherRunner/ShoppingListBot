343614ca97f73fbdf538e539d8b5f94f94239319
CipherRunner <example@gmail.com>
2024-01-19 21:21:04 +0100

    Menu added

--- src/main/java/com/shoppingbot/SpringBot/Service/TelegramBot.java
+++ src/main/java/com/shoppingbot/SpringBot/Service/TelegramBot.java
@@ -4,18 +4,38 @@ import com.shoppingbot.SpringBot.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
+    final String HELP_TEXT = "Help text";
+
     public TelegramBot(BotConfig botConfig) {
         this.config = botConfig;
-    }
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "start the bot"));
+        listOfCommands.add(new BotCommand("/mydata", "get your stored data"));
+        listOfCommands.add(new BotCommand("/removedata", "delete your stored data"));
+        listOfCommands.add(new BotCommand("/help", "how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "configure your preferences"));
 
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
 
 
     @Override
@@ -36,6 +56,18 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChatId(), update.getMessage().getChat().getFirstName());
+                    break;
+                case "/mydata":
+                    startCommandReceived(chatId, update.getMessage().getChatId(), update.getMessage().getChat().getFirstName());
+                    break;
+                case "/deletedata":
+                    startCommandReceived(chatId, update.getMessage().getChatId(), update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                case "/settings":
                     startCommandReceived(chatId, update.getMessage().getChatId(), update.getMessage().getChat().getFirstName());
                     break;
                 default:
